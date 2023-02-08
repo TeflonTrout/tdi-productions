745944c92882ca3d99eab9682a870cccb9774b54
TeflonTrout <jtkazo@gmail.com>
2023-02-07 20:42:59 -0500

    continuing to wireframe carousel and cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
-import { Button, Carousel, SimpleCard } from '@/components'
+import { Button, Navbar, SimpleCard } from '@/components'
+import { Carousel } from "react-responsive-carousel"
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Link from 'next/link'
 
 export default function Home() {
@@ -12,7 +14,45 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Carousel />
+      <Navbar />
+      <div style={{width: '100%', display: 'flex', justifyContent: "center"}}>
+        <Carousel 
+          infiniteLoop={true}
+          interval={8000}
+          showArrows={false} 
+          showStatus={false} 
+          autoPlay={true} 
+          showThumbs={false} 
+          renderIndicator={(onClickHandler, isSelected, index, label) => {
+            const defStyle = {display: "inline-block", cursor: "pointer", margin: "0 20px 0 20px", justifyContent: "center", backgroundColor: "grey", width: "100px", height: "10px", borderRadius: "5px" };
+            const style = isSelected
+            ? { ...defStyle, backgroundColor: "white" }
+            : { ...defStyle };
+            return (
+              <span
+                style={style}
+                onClick={onClickHandler}
+                onKeyDown={onClickHandler}
+                key={index}
+                role="button"
+                tabIndex={0}
+                aria-label={`${label} ${index + 1}`}
+              >
+              </span>
+            );
+          }}
+          className={styles.carousel}>
+          <div>
+            <img src="/thumbnails/insideTheTent.png" />
+          </div>
+          <div>
+            <img src="/thumbnails/pigs.jpg" />
+          </div>
+          <div>
+            <img src="/thumbnails/confinement.jpg" />
+          </div>
+        </Carousel>
+      </div>
       <div className={styles.hero}>
         <h1>Latest Releases</h1>
         <div className={styles.cardGrid}>
