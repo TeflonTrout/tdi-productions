033a040fc84afa2301d8e4c50bad9c5e9876b104
TeflonTrout <jt@joy.world>
2023-02-16 11:08:07 -0500

    Update footer to include FontAwesome Icons along with better styling for UX

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,37 +1,35 @@
+import { useState } from "react"
 import styles from "./Footer.module.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faYoutube, faInstagram, faTiktok } from "@fortawesome/free-brands-svg-icons"
+import Link from "next/link"
 
 const Footer = () => {
+  const [hover, setHover] = useState<string>("");
   return (
     <div className={styles.footer}>
       <div className={styles.logoContainer}>
         <img src="/tdiLogo.png" alt="tdi production" width="150px"/>
         <div className={styles.socials}>
-          <p>YT</p>
-          <p>IG</p>
-          <p>TT</p>
+          <FontAwesomeIcon className={styles.icon} size="xs" icon={faYoutube}  />
+          <FontAwesomeIcon className={styles.icon} size="xs" icon={faInstagram} />
+          <FontAwesomeIcon className={styles.icon} size="xs" icon={faTiktok} />
         </div>
       </div>
       <div className={styles.linkContainer}>
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Films</li>
-          <li>Contact</li>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/about">About</Link></li>
+          <li><Link href="/films">Films</Link></li>
+          <li><Link href="/contact">Contact</Link></li>
         </ul>
       </div>
       <div className={styles.linkContainer}>
         <ul>
           <li>YouTube</li>
+          <li>Twitter</li>
           <li>TikTok</li>
           <li>Instagram</li>
-        </ul>
-      </div>
-      <div className={styles.linkContainer}>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Films</li>
-          <li>Contact</li>
         </ul>
       </div>
     </div>
