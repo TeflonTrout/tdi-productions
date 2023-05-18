9bcc5d36037809bacdc6b19abc3462ea953f74eb
Nicholas <nek317@gmail.com>
2023-05-18 14:53:38 -0400

    contact page style updates

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -7,7 +7,7 @@ const index = () => {
         <h1>Contact</h1>
         <div className={styles.hero}>
           <div className={styles.container}>
-            <h1>Email Us: *insert email here*</h1>
+            <h1>Email Us: <a href='mailto:20dollarinsuranceproduction@gmail.com'>20DollarInsuranceProduction@gmail.com</a></h1>
           </div>
           <div className={styles.container}>
             <h1><a href="https://twitter.com/TDI_Productions" target="_blank" rel="noopener noreferrer">Twitter</a></h1>
